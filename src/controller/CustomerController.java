7ac2e8c6f3ad4f3d494e3341c466afc243c0a59f
NuwanS rajapaksha <155893660+NuwanSrajapasha@users.noreply.github.com>
2024-12-22 00:57:04 +0530

    CustomerController

--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -1,5 +1,32 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class CustomerController {
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtID;
+
+    @FXML
+    private TextField txtTitle;
+
+    @FXML
+    private TextField txtname;
+
+    @FXML
+    void btnOnAction(ActionEvent event) {
+        System.out.println("save btn working");
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+        System.out.println("save btn working");
+
+    }
 
 }
